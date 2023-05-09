42c94a50e0239e1395bfa0dc2c3b99e2a6534cf0
Dhrupal Gajjar <harmit.mendapara@aliansoftware.net>
2023-05-09 15:42:54 +0530

    added template script

--- post-init.script.js
+++
@@ -1,6 +0,0 @@
-const { rm } = require('fs').promises;
-const { applyPlugins } = require('./template/plugins');
-
-applyPlugins().then(async () => {
-  await rm('./plugins', { recursive: true });
-});
