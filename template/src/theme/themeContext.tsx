839217073b23056f8c06bd720f5adfcb7024fa36
Dhrupal Gajjar <harmit.mendapara@aliansoftware.net>
2023-05-09 09:57:20 +0530

    theme setup completed

--- template/src/theme/themeContext.tsx
+++ template/src/theme/themeContext.tsx
@@ -5,25 +5,3 @@ export const ThemeContext = React.createContext({
     theme: lightTheme,
     setTheme: () => { }
 })
-
-// export const ThemeContextProvider = (props: any) => {
-
-//     const setTheme = (theme: any) => {
-//         setState({ ...state, theme: theme })
-//     }
-
-//     const initState = {
-//         theme: lightTheme,
-//         setTheme: setTheme
-//     }
-
-//     const [state, setState] = useState(initState)
-
-//     return (
-//         <ThemeContext.Provider value={state}  >
-//         { props.children }
-//         </ThemeContext.Provider>
- 
- 
-//   )
-// }
