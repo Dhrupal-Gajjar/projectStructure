6f7834ff4e7337d13f28c6cf8bfb0b957f529817
Dhrupal Gajjar <harmit.mendapara@aliansoftware.net>
2023-05-09 12:24:51 +0530

    project structure folder added

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -2,9 +2,8 @@ import * as React from 'react';
 import { useColorScheme } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
 
-import App from './screens/login';
-import { darkTheme, lightTheme } from './theme';
-import {  ThemeContext } from './theme/themeContext'
+import App from 'screens/login';
+import { darkTheme, lightTheme,ThemeContext } from 'theme';
 export default function Main() {
     const scheme = useColorScheme();
     const state = React.useContext(ThemeContext)
